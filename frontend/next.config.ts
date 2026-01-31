9a2f833da155237c9c353d5a44688414e5d7c6b7
hnaitoMac <naitoh8@gmail.com>
2026-02-01 01:23:41 +0900

    fix: add conditional basePath and assetPrefix for production in Next.js config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
+const repoName = "github-pages";
+
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+
+  ...(isProd && {
+    basePath: `/${repoName}`,
+    assetPrefix: `/${repoName}/`,
+  }),
+
   images: {
     unoptimized: true,
   },
